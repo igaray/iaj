06db7478fd8654630f5724fa8d1eab9d897cabd9
Leo Molas <leos.molas@gmail.com>
2013-01-27 17:39:23 -0300

    now the gold is added programmatically

--- unity/IAJ/Gold.cs
+++ unity/IAJ/Gold.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Gold : EObject {
 	
+	// creates an instance of the prefab Gold, that has this script attached, 
+	// and returns the instance of this class that is attached to the object,
+	// initialized
 	public static Gold Create(	Object  prefab, 
 								Vector3 position, 
 								string  description, 
