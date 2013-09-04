8cefb399e33cb000ee37c915359cb25452d1671b
MauroGomez <mauro@buzzhives.com>
2013-09-04 11:29:16 -0300

    Normalize line endings

---
+++ unity/IAJ/Assets/Code/GUIClasses/InfoControlBar.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class InfoControlBar : MonoBehaviour
+{
+	
+	void OnGUI () {
+		GUI.Window(0, new Rect(Screen.width - 170, 20, 160, 300), WindowFunction, "Game Control / Info");		
+	}
+	
+	public GUIStyle timeLabelStyle;
+	
+	void WindowFunction(int windowId) {				
+		GUILayout.BeginVertical();
+			GUILayout.Label(SimulationState.getInstance().gameTime.ToString(), timeLabelStyle);
+			foreach (AgentState agentState in SimulationState.getInstance().agents.Values) {
+				AgentPanel(agentState.agentController);
+			}
+		GUILayout.EndVertical();
+	}
+	
+	public Texture2D goldIcon;
+		
+	void AgentPanel(Agent agent) {		
+		GUILayout.BeginVertical();	
+			//GUILayout.Box(agent._name, GUILayout.Height(100f));			
+		    GUILayout.Box(agent._name);			
+			GUILayout.Label("skill: "+agent.skill);
+			GUILayout.BeginHorizontal();
+			 GUILayout.Label("backpack:");		
+		GUILayout.Label(goldIcon);
+			 foreach (EObject obj in agent.backpack) {
+		 		GUILayout.Label(goldIcon);
+			 }
+			GUILayout.EndHorizontal();
+		GUILayout.EndVertical();
+	}
+}
+
---
+++ unity/IAJ/Assets/Scenes/GameInfoControGUI.js
@@ -0,0 +1,20 @@
+#pragma strict
+
+
+function OnGUI () {
+
+	//GUI.Window (0, Rect(630, 20, 160, 300), WindowFunction, "Game Control / Info");
+
+}
+
+function WindowFunction() {
+	//GUI.Label(Rect(70,25,40,60), );
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
