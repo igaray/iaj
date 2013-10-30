7b87d041c4318e3280edfbafc7ffcae1c28f60b4
MauroGomez <mauro@buzzhives.com>
2013-10-30 11:32:40 -0300

    added scroll to info panel, added life to agent info

--- unity/IAJ/Assets/Code/GUIClasses/InfoControlBar.cs
+++ unity/IAJ/Assets/Code/GUIClasses/InfoControlBar.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class InfoControlBar : MonoBehaviour
 {
+	Vector2 scrollPosition;
 	
 	void OnGUI () {
-		GUI.Window(0, new Rect(Screen.width - 170, 20, 160, 300), WindowFunction, "Game Control / Info");		
+		GUI.Window(0, new Rect(Screen.width - 190, 20, 180, 300), WindowFunction, "Game Control / Info");		
 	}
 	
 	public GUIStyle timeLabelStyle;
@@ -13,9 +14,11 @@ public class InfoControlBar : MonoBehaviour
 	void WindowFunction(int windowId) {				
 		GUILayout.BeginVertical();
 			GUILayout.Label(SimulationState.getInstance().gameTime.ToString(), timeLabelStyle);
+			scrollPosition = GUILayout.BeginScrollView(scrollPosition);
 			foreach (AgentState agentState in SimulationState.getInstance().agents.Values) {
 				AgentPanel(agentState.agentController);
 			}
+			GUILayout.EndScrollView();
 		GUILayout.EndVertical();
 	}
 	
@@ -25,7 +28,7 @@ public class InfoControlBar : MonoBehaviour
 		GUILayout.BeginVertical();	
 			//GUILayout.Box(agent._name, GUILayout.Height(100f));			
 		    GUILayout.Box(agent._name);			
-			GUILayout.Label("skill: "+agent.skill);
+			GUILayout.Label("HP: "+agent.life+"/"+agent.lifeTotal+"  "+"XP: "+agent.skill);
 			GUILayout.BeginHorizontal();
 			 GUILayout.Label("backpack:");				
 			 foreach (EObject obj in agent.backpack) {
